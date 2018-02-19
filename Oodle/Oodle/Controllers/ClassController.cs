9cd3d423d9b881bf966c4fe46afc102a1537088a
William Leingang <wleingang15@mail.wou.edu>
2018-02-19 14:12:55 -0800

    Working on view switching.

---
+++ Oodle/Oodle/Controllers/ClassController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Oodle.Models.ViewModels;
+
+namespace Oodle.Controllers
+{
+    public class ClassController : Controller
+    {
+        private UserRoleVM vm = new UserRoleVM();
+        // GET: Class
+        public ActionResult Index()
+        {
+            var idid = User.Identity.GetUserId();
+            
+
+
+            return View();
+        }
+    }
+}
