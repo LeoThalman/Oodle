3813f0bdbf380c714e046fc3b8254162b605e868
William Leingang <wleingang15@mail.wou.edu>
2018-02-20 15:26:16 -0800

    Worked more on class views and list of classes.

--- Oodle/Oodle/Controllers/ClassController.cs
+++ Oodle/Oodle/Controllers/ClassController.cs
@@ -26,7 +26,18 @@ namespace Oodle.Controllers
             if (urc.RoleID == 0) {
                 RedirectToAction("Teacher", classID);
             }
-
+            else if (urc.RoleID == 1)
+            {
+                RedirectToAction("Grader", classID);
+            }
+            else if (urc.RoleID == 2)
+            {
+                RedirectToAction("Student", classID);
+            }
+            else if (urc.RoleID == 3)
+            {
+                RedirectToAction("Pending", classID);
+            }
             return View();
         }
 
@@ -42,6 +53,70 @@ namespace Oodle.Controllers
                 RedirectToAction("Index", classID);
             }
             return View();
+        }
+
+        public ActionResult Grader(int classID)
+        {
+            var idid = User.Identity.GetUserId();
+
+            User user = db.Users.Where(a => a.IdentityID == idid).FirstOrDefault();
+            UserRoleClass urc = db.UserRoleClasses.Where(s => s.UsersID == user.UsersID && s.ClassID == classID).FirstOrDefault();
+
+            if (urc.RoleID != 1)
+            {
+                RedirectToAction("Index", classID);
+            }
+            return View();
+        }
+
+        public ActionResult Student(int classID)
+        {
+            var idid = User.Identity.GetUserId();
+
+            User user = db.Users.Where(a => a.IdentityID == idid).FirstOrDefault();
+            UserRoleClass urc = db.UserRoleClasses.Where(s => s.UsersID == user.UsersID && s.ClassID == classID).FirstOrDefault();
+
+            if (urc.RoleID != 2)
+            {
+                RedirectToAction("Index", classID);
+            }
+            return View();
+        }
+
+        public ActionResult Pending(int classID)
+        {
+            var idid = User.Identity.GetUserId();
+
+            User user = db.Users.Where(a => a.IdentityID == idid).FirstOrDefault();
+            UserRoleClass urc = db.UserRoleClasses.Where(s => s.UsersID == user.UsersID && s.ClassID == classID).FirstOrDefault();
+
+            if (urc.RoleID != 3)
+            {
+                RedirectToAction("Index", classID);
+            }
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public void CreateClass()
+        {
+            var idid = User.Identity.GetUserId();
+            User user = db.Users.Where(a => a.IdentityID == idid).FirstOrDefault();
+
+
+
+
+
+            //db.Classes.Add(c);
         }
     }
 }
