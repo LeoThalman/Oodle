f636ab25b18be3d9cfa36e53143b221fa133b855
William Leingang <wleingang15@mail.wou.edu>
2018-02-20 06:14:25 -0800

    Getting ready for a push.

--- Oodle/Oodle/Controllers/ClassController.cs
+++ Oodle/Oodle/Controllers/ClassController.cs
@@ -4,20 +4,43 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
-using Oodle.Models.ViewModels;
+using Oodle.Models;
 
 namespace Oodle.Controllers
 {
     public class ClassController : Controller
     {
-        private UserRoleVM vm = new UserRoleVM();
+        private Model1 db = new Model1();
+
+
         // GET: Class
-        public ActionResult Index()
+        public ActionResult Index(int classID)
         {
             var idid = User.Identity.GetUserId();
-            
+
+            User user = db.Users.Where(a => a.IdentityID == idid).FirstOrDefault();
+
+            UserRoleClass urc = db.UserRoleClasses.Where(s => s.UsersID == user.UsersID && s.ClassID == classID).FirstOrDefault();
 
 
+            if (urc.RoleID == 0) {
+                RedirectToAction("Teacher", classID);
+            }
+
+            return View();
+        }
+
+        public ActionResult Teacher(int classID)
+        {
+            var idid = User.Identity.GetUserId();
+
+            User user = db.Users.Where(a => a.IdentityID == idid).FirstOrDefault();
+            UserRoleClass urc = db.UserRoleClasses.Where(s => s.UsersID == user.UsersID && s.ClassID == classID).FirstOrDefault();
+
+            if (urc.RoleID != 0)
+            {
+                RedirectToAction("Index", classID);
+            }
             return View();
         }
     }
