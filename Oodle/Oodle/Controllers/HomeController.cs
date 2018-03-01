c9e47b9ad3d4ceaef3c933e8181873158dc35ae9
kollklienstuber <kklienstuber15@mail.wou.edu>
2018-02-28 18:34:34 -0800

    added FAQ, changed font and some styling

--- Oodle/Oodle/Controllers/HomeController.cs
+++ Oodle/Oodle/Controllers/HomeController.cs
@@ -20,11 +20,6 @@ namespace Oodle.Controllers
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+  
     }
 }
