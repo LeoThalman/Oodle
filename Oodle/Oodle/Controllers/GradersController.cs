f0d58d1139eda977873d88e04ba2683221f68aa6
Sam Wetzel <samwow13@yahoo.com>
2018-02-28 17:21:46 -0800

    added shared layouts for students, teachers, graders

---
+++ Oodle/Oodle/Controllers/GradersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Oodle.Controllers
+{
+    public class GradersController : Controller
+    {
+        // GET: Graders
+        public ActionResult Index()
+        {
+            return View("Index", "_GraderLayout");
+        }
+
+        public ActionResult ToDoList()
+        {
+            return View("ToDoList", "_GraderLayout");
+        }
+
+        public ActionResult GradeAssignment()
+        {
+            return View("GradeAssignment", "_GraderLayout");
+        }
+
+        public ActionResult GradeQuiz()
+        {
+            return View("GradeQuiz", "_GraderLayout");
+        }
+
+        public ActionResult GradeTask()
+        {
+            return View("GradeTask", "_GraderLayout");
+        }
+    }
+}
