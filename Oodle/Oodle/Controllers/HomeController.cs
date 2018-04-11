4b4d437cdbff5924e5d6a7e009633d6f1e3b417d
kollklienstuber <kklienstuber15@mail.wou.edu>
2018-04-11 13:54:00 -0700

    added in my tests, minor fixes to styling, added testing tests should see one failed test when running that is named 'should fail'

--- Oodle/Oodle/Controllers/HomeController.cs
+++ Oodle/Oodle/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,6 +14,7 @@ namespace Oodle.Controllers
             return View();
         }
 
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,6 +22,13 @@ namespace Oodle.Controllers
             return View();
         }
 
+        public class FormData
+        {
+            public string TextBoxStringData { get; set; }
+
+            public int TextBoxIntData { get; set; }
+
+        }
 
         public ActionResult Tools()
         {
@@ -27,11 +36,48 @@ namespace Oodle.Controllers
             return View();
         }
 
+
+        [HttpPost]
+        public int Tools(int textBoxIntData = 5)
+        {
+            int rating = textBoxIntData;
+            //if rating is less then 0 we will just consider it the lowest possible
+            if (rating < 0)
+            {
+                rating = 0;
+            } //if rating is above 5 then we will just make it max rating.
+            else if (rating > 5)
+            {
+                rating = 5;
+            }
+            //if its between 0-5 then we will keep it as is
+              
+
+            System.Diagnostics.Debug.WriteLine("this should only ever be between 1 and 5" + rating);
+
+            return rating;
+        }
+
+       
+
         public ActionResult UnderDevelopment()
         {
 
             return View();
+
         }
+
+       
+
+
+        //can remove, was just using to test that testing works and it does
+        public string Capitalize(string str)
+        {
+            return char.ToUpper(str[0]) + str.Substring(1);
+
+        }
+
+
 
 
 
--- Oodle/Test/UnitTest1.cs
+++ Oodle/Test/UnitTest1.cs
@@ -9,11 +9,65 @@ namespace Test
     public class UnitTest1
     {
         [Test]
-        public void TestMethod1()
+        public void TestingTestShouldPass()
         {
+            HomeController c = new HomeController();
+            string input = "test";
+            string expected = "Test";
+            string result = c.Capitalize(input);
+            Assert.That(result, Is.EqualTo(expected));
+            
 
+        }
+
+
+        [Test]
+        public void TestingTestShouldFail()
+        {
+            HomeController c = new HomeController();
+            string input = "notFail";
+            string expected = "Fail";
+            string result = c.Capitalize(input);
+            Assert.That(result, Is.EqualTo(expected));
 
 
         }
+
+        [Test]
+        public void kollsOodleRatingTestOnToolsPageShouldBeGreaterThanOrEqualTo0()
+        {
+            HomeController rating = new HomeController();
+
+            int lowestPossibleScore = 0;
+            int highestPossibleScore = 5;
+
+            //Testing input of 0 to be a valid number
+            int EdgeCaseOfZero = 0;
+            int numberReturned = rating.Tools(EdgeCaseOfZero);
+            Assert.That(numberReturned >= lowestPossibleScore);
+
+
+            //Testing input of negative to be changed to 0
+            int negativeNumber = -1;
+            numberReturned = rating.Tools(negativeNumber);
+            Assert.That(numberReturned >= lowestPossibleScore);
+
+
+            //Testing input of anything over 5 to be changed to a 5
+            int upperboundNumber = 7;
+            numberReturned = rating.Tools(upperboundNumber);
+            Assert.That(numberReturned <= highestPossibleScore);
+
+
+
+            //Testing input of anything over 5 to be changed to a 5
+            int ValidNumber = 1;
+            numberReturned = rating.Tools(ValidNumber);
+            Assert.That(numberReturned <= highestPossibleScore && numberReturned >=lowestPossibleScore);
+
+
+        }
+
+
     }
 }
