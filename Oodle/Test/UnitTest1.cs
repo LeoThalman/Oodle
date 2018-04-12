41ff59032d94a667bcea79bf96e05d418d6caa70
LeoThalman <lthalman15@wou.edu>
2018-04-11 21:38:25 -0700

    Added Test project and unit tests

--- Oodle/Test/UnitTest1.cs
+++ Oodle/Test/UnitTest1.cs
@@ -16,7 +16,7 @@ namespace Test
             string expected = "Test";
             string result = c.Capitalize(input);
             Assert.That(result, Is.EqualTo(expected));
-            
+
 
         }
 
@@ -25,7 +25,7 @@ namespace Test
         public void TestingTestShouldFail()
         {
             HomeController c = new HomeController();
-            string input = "notFail";
+            string input = "notFail"; 
             string expected = "Fail";
             string result = c.Capitalize(input);
             Assert.That(result, Is.EqualTo(expected));
@@ -63,11 +63,61 @@ namespace Test
             //Testing input of anything over 5 to be changed to a 5
             int ValidNumber = 1;
             numberReturned = rating.Tools(ValidNumber);
-            Assert.That(numberReturned <= highestPossibleScore && numberReturned >=lowestPossibleScore);
+            Assert.That(numberReturned <= highestPossibleScore && numberReturned >= lowestPossibleScore);
 
 
         }
 
+        [Test]
+        public void ValidateSlackName_ShortensNamesLongerThan21Chars_ReturnsShortenedName()
+        {
+            SlackController slack = new SlackController();
+            string temp = "thisnameislongerthan21chars";
+            string answer = "thisnameislongerthan2";
+            string temp2 = "thisnameis-longenough";
 
+            Assert.That(slack.ValidateSlackName(temp), Does.Match(answer));
+            Assert.That(slack.ValidateSlackName(temp2), Does.Match(temp2));
+        }
+
+        [Test]
+        public void ValidateSlackName_NullParamReturnsEmptyString_ReturnsEmptyString()
+        {
+            SlackController slack = new SlackController();
+            string temp = null;
+            string answer = "";
+            Assert.That(slack.ValidateSlackName(temp), Does.Match(answer));
+        }
+
+        [Test]
+        public void ValidateSlackName_ConvertsSpacesToDashes_ReturnsConvertedName()
+        {
+            SlackController slack = new SlackController();
+            string temp = "a  a";
+            string answer = "a--a";
+            Assert.That(slack.ValidateSlackName(temp), Does.Match(answer));
+        }
+
+        [Test]
+        public void ValidateSlackName_ConvertsToLowerCase_ReturnsConvertedName()
+        {
+            SlackController slack = new SlackController();
+            string temp = "TEST";
+            string answer = "test";
+
+            Assert.That(slack.ValidateSlackName(temp), Does.Match(answer));
+        }
+
+        [Test]
+        public void ValidateSlackName_ConvertsSpecialCharToUnderScore_ReturnsConverted()
+        {
+            SlackController slack = new SlackController();
+            string temp = "!@#$%^&()+=|><,.;:[]{}";
+            string answer = "_";
+
+            Assert.That(slack.ValidateSlackName(temp), Does.Match(answer));
+
+
+        }
     }
 }
