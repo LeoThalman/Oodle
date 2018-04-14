f456bd60826f428fdbf35e13d2be2f229ebd2da3
LeoThalman <lthalman15@wou.edu>
2018-04-13 20:33:09 -0700

    Merging in development

---
+++ Oodle/Oodle/Models/ViewModels/TeacherVM_LOCAL_11556.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Oodle.Models.ViewModels
+{
+    public class TeacherVM
+    {
+        public Class cl { get; set; }
+        public List<Assignment> assignment { get; set; }
+        public List<User> users { get; set; }
+        public List<Document> documents {get; set;}
+        public List<ClassNotification> notifs { get; set; }
+        public List<int> classGrade { get; set; }
+
+        public TeacherVM(Class classP, List<User> userP)
+        {
+            cl = classP;
+            users = userP;
+        }
+    }
+}
